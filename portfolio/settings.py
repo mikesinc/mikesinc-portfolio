20c021e75328ba5b215b02047669a02542693138
mikesinc <mas.sinclair@gmail.com>
2020-06-23 21:09:50 +0800

    heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,16 +115,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'media')
+    # os.path.join(BASE_DIR, 'build/static'),
+    # os.path.join(BASE_DIR, 'media')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'build/media')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
